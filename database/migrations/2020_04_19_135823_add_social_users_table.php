eeb03c89ee0443ec9a95df6cf84b978cc822f740
林品爵 <javck.zack@gmail.com>
2020-04-19 14:06:15 +0800

    Laravel實戰篇  users表格新增provider和provider_id

---
+++ database/migrations/2020_04_19_135823_add_social_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSocialUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('provider',50)->nullable();
+            $table->string('provider_id',100)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('provider');
+            $table->dropColumn('provider_id');
+        });
+    }
+}
